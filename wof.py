67806281274ee4b8aed979226fa72ebb4e56b73c
Brian Myers <brian.myers@kampspallets.com>
2024-02-29 13:58:37 -0500

    Initial commit WOF

---
+++ wof.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import os
+
+st.sidebar.write("Hello ",os.getlogin())
+
+st.image("https://www.thewordfinder.com/wof-puzzle-generator/puzzle.php?bg=1&ln1=Streamlit&ln2=Wheel of&ln3=Fortune&ln4=&cat=SHOW TITLE&")
