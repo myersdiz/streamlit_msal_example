96f1ba74d6df1b355209f2be7a2baacd40bfd311
Brian Myers <brian.myers@kampspallets.com>
2024-03-01 11:06:12 -0500

    Added authentication using streamlit_msal

--- wof.py
+++ wof.py
@@ -1,30 +1,44 @@
 import streamlit as st
-from streamlit.web.server.websocket_headers import _get_websocket_headers
-from streamlit_javascript import st_javascript
+from streamlit_msal import Msal
 
-headers = _get_websocket_headers()
-access_token = headers.get("X-Access-Token")
-st.write(headers)
+import os
 
-st.write(st.experimental_user.email)
+client_id = os.getenv("AZURE_CLIENT_ID")
+authority = os.getenv("AZURE_AUTHORITY")
 
-def read_aad_username():
-    js_code = """(await fetch("/.auth/me")
-                    .then(function(response) {return response.json();}).then(function(body) {return body;}))"""
+with st.sidebar:
+    auth_data = Msal.initialize_ui(
+        client_id=client_id,
+        authority=authority,
+        scopes=[], # Optional
+        # Customize (Default values):
+        connecting_label="Connecting",
+        disconnected_label="Click <Sign in> to authenticate",
+        sign_in_label="Sign in",
+        sign_out_label="Sign out"
+    )
 
-    return_value = st_javascript(js_code)
+if not auth_data:
+    st.write("Authenticate to access protected content")
+    st.stop()
 
-    username = None
-    if return_value == 0:
-        pass # this is the result before the actual value is returned 
-    elif isinstance(return_value, list) and len(return_value) > 0:
-        username = return_value[0]["user_id"]
-        st.write(f"Logged in as {username}")
-    else:
-        st.warning(f"could not directly read username from azure active directory: {return_value}.")
-        st.warning(f"A workaround to this is to clear your browser cookies for this site and reloading it.")
-    return username
+if st.sidebar.button("Revalidate"):
+    Msal.revalidate() # Usefull to refresh "accessToken"
 
-st.write(read_aad_username())
+access_token = auth_data["accessToken"]
 
-st.image("https://www.thewordfinder.com/wof-puzzle-generator/puzzle.php?bg=1&ln1=Streamlit&ln2=Wheel of&ln3=Fortune&ln4=&cat=SHOW TITLE&")
+account = auth_data["account"]
+name = account["name"]
+username = account["username"]
+account_id = account["localAccountId"]
+
+st.sidebar.write(f"Hello {name}!")
+st.sidebar.write(f"Your username is: {username}")
+st.sidebar.write(f"Your account id is: {account_id}")
+st.sidebar.write("Your access token is:")
+st.sidebar.code(access_token)
+
+st.image("https://www.thewordfinder.com/wof-puzzle-generator/puzzle.php?bg=1&ln1=Streamlit&ln2=Kamps&ln3=Pallets&ln4=&cat=SHOW TITLE&")
+
+st.write("Auth data:")
+st.json(auth_data)
